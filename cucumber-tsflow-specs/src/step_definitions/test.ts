c8d57478d67425f43b89148b01a1c8a94c5eaa42
Kelly Ferrone <4399427+kferrone@users.noreply.github.com>
2020-05-12 12:57:17 -0600

    allowed any tag expression in hooks

--- cucumber-tsflow-specs/src/step_definitions/test.ts
+++ cucumber-tsflow-specs/src/step_definitions/test.ts
@@ -1,9 +1,12 @@
 import { equal } from "assert";
-import { before, binding, given } from "cucumber-tsflow";
+import { after, before, binding, given, then, when } from "cucumber-tsflow";
 
 class Foo {
   public readonly actual = true;
 }
+
+let afterBazCount = 0;
+let afterCount = 0;
 
 // tslint:disable-next-line:max-classes-per-file
 @binding([Foo])
@@ -22,4 +25,27 @@ export default class TestSteps {
     await equal(this.actual, true);
     await equal(this.foo.actual, true);
   }
+
+  @when("bar")
+  public WhenBar(): void {
+    equal(1+1, 2, "This is true");
+  }
+
+  @then("baz")
+  public ThenBaz(): void {
+    equal(false, false, "This is false");
+  }
+
+  @after("not @baz")
+  public after() {
+    ++afterCount;
+    equal((afterCount < 3), true, "It's true, I ran");
+  }
+
+  @after("@baz")
+  public afterBazOnly() {
+    ++afterBazCount;
+    equal(afterBazCount, 1, "This should only ever be one");
+  }
+
 }
