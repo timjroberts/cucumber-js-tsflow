02e5450c0047a73f1934803bc2d1925830d7337a
Wudong Liu <wudong.liu@gmail.com>
2020-07-27 13:53:57 +0100

    upgrade packages and reorganize test

---
+++ cucumber-tsflow-specs/features/tagging-test.feature
@@ -0,0 +1,14 @@
+@tagging
+Feature: Test Features with Tagging support.
+
+Because 'basic-test.ts' step definition is not tagged,
+the step definition will be used here as well.
+
+
+Scenario: Basic test scenario
+    Given some step to be executed
+    When the condition is right
+    Then we can see the result correctly
+
+
+
---
+++ cucumber-tsflow-specs/src/step_definitions/basic-test.ts
@@ -0,0 +1,65 @@
+import { after, before, binding, given, then, when } from "cucumber-tsflow";
+import * as expect from "expect";
+
+@binding()
+export default class TestSteps {
+  private givenIsCalled = false;
+  private beforeIsCalled = false;
+  private whenIsCalled = false;
+  private thenIsCalled = false;
+  private beforeWithNoTagIsCalled = false;
+
+  @before()
+  public beforeWithNoTag() {
+    this.beforeWithNoTagIsCalled = true;
+  }
+
+  @before("@basic")
+  public before() {
+    this.beforeIsCalled = true;
+  }
+
+  @after("@basic")
+  public after() {
+    expect(this.beforeWithNoTagIsCalled).toBe(true);
+    expect(this.beforeIsCalled).toBe(true);
+    expect(this.whenIsCalled).toBe(true);
+    expect(this.givenIsCalled).toBe(true);
+    expect(this.thenIsCalled).toBe(true);
+    // tslint:disable-next-line:no-console
+    console.log("@basic after hook is called.");
+  }
+
+  @after("@tagging")
+  public afterForTagging() {
+    // this is not called by tagging feature.
+    expect(this.beforeIsCalled).toBe(false);
+    // tslint:disable-next-line:no-console
+    console.log("@tagging after hook is called.");
+  }
+
+  @after()
+  public afterWithoutTag() {
+    expect(this.beforeWithNoTagIsCalled).toBe(true);
+    expect(this.whenIsCalled).toBe(true);
+    expect(this.givenIsCalled).toBe(true);
+    expect(this.thenIsCalled).toBe(true);
+    // tslint:disable-next-line:no-console
+    console.log("no tag after hook is called.");
+  }
+
+  @given(/some step to be executed/)
+  public givenSomeStepTobeExecuted() {
+    this.givenIsCalled = true;
+  }
+
+  @when(/the condition is right/)
+  public whenTheConditionIsRight() {
+    this.whenIsCalled = true;
+  }
+
+  @then(/we can see the result correctly/)
+  public thenWeCanSeeTheResult() {
+    this.thenIsCalled = true;
+  }
+}
