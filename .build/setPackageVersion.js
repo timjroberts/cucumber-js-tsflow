e752a519b24479d893a2a92d3064a668f60f9b2a
Tim Roberts <tim@timjroberts.com>
2020-11-08 12:17:10 +0000

    Update CI workflow to pack and publish

--- .build/setPackageVersion.js
+++ .build/setPackageVersion.js
@@ -1,9 +1,11 @@
 const nbgv = require("nerdbank-gitversioning");
 
 const setPackageVersionAndBuildNumber = (versionInfo) => {
-    console.log("Setting package version to " + versionInfo.npmPackageVersion);
+    // Set a build output value representing the NPM package version
+    console.log("::set-output name=package_version::" + versionInfo.npmPackageVersion);
     
     nbgv.setPackageVersion("cucumber-tsflow");
+    nbgv.setPackageVersion("cucumber-tsflow-specs");
 };
 
 const handleError = (err) => console.error("Failed to update the package version number. nerdbank-gitversion failed: " + err);
