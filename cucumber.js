281505ae730620693affb3557f8ab5e3cb695ce6
Luiz Ferraz <luiz@lferraz.com>
2022-11-19 20:57:46 -0300

    Fix test configuration for cucumber 7

--- cucumber.js
+++ cucumber.js
@@ -1,9 +1,19 @@
-module.exports = {
-  default: {
-    publishQuiet: true,
-    require: ['cucumber-tsflow-specs/dist/**/*.js'],
-    worldParameters: {
-      foo: 'bar',
-    }
-  },
-};
+const cucumberPkg = require('@cucumber/cucumber/package.json');
+
+module.exports = cucumberPkg.version.startsWith('7.')
+  ? {
+    default: [
+      '--publish-quiet',
+      '--require cucumber-tsflow-specs/dist/**/*.js',
+      '--world-parameters \'{"foo":"bar"}\'',
+    ].join(' ')
+  }
+  : {
+    default: {
+      publishQuiet: true,
+      require: ['cucumber-tsflow-specs/dist/**/*.js'],
+      worldParameters: {
+        foo: 'bar',
+      }
+    },
+  };
